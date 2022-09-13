ae8881c6d761992a669bf04e23c22366681636f6
rubenleal89 <rubendarioleal618@gmail.com>
2022-09-13 18:00:02 -0500

    feat add files codeHotel.js, styles.css and index.html

---
+++ codeHotel.js
@@ -0,0 +1,164 @@
+let arrayReview = [
+    {
+        "id": 1,
+        "hotelId": 1,
+        "title": "Best holiday ever",
+        "description": "Donec a nisi in mi pellentesque placerat vel eu leo. Nulla facilisi. Duis dui nulla, ornare sed efficitur vitae, aliquam vel tortor. Nunc porta varius ex. Donec id porta lacus, ac facilisis nulla. Proin sed felis nec tellus dictum commodo nec quis lorem. Nam ultrices, risus ut maximus ullamcorper, elit tellus tincidunt tortor, eu euismod turpis ipsum et leo.",
+        "rating": 5
+      },
+      {
+        "id": 2,
+        "hotelId": 1,
+        "title": "Very clean",
+        "description": "Duis blandit, dolor sed posuere sodales, diam lorem tempor libero, at vestibulum turpis nisl porttitor enim. Cras accumsan felis orci, a sagittis lectus porta ut.",
+        "rating": 4
+      },
+      {
+        "id": 5,
+        "hotelId": 2,
+        "title": "Exceeded all expectations",
+        "description": "Donec a nisi in mi pellentesque placerat vel eu leo. Nulla facilisi. Duis dui nulla, ornare sed efficitur vitae, aliquam vel tortor. Nunc porta varius ex. Donec id porta lacus, ac facilisis nulla. Proin sed felis nec tellus dictum commodo nec quis lorem. Nam ultrices, risus ut maximus ullamcorper, elit tellus tincidunt tortor, eu euismod turpis ipsum et leo.",
+        "rating": 5
+      },
+      {
+        "id": 6,
+        "hotelId": 2,
+        "title": "Very clean",
+        "description": "Duis blandit, dolor sed posuere sodales, diam lorem tempor libero, at vestibulum turpis nisl porttitor enim. Cras accumsan felis orci, a sagittis lectus porta ut.",
+        "rating": 4
+      },
+      {
+        "id": 8,
+        "hotelId": 2,
+        "title": "Very clean",
+        "description": "Duis blandit, dolor sed posuere sodales, diam lorem tempor libero, at vestibulum turpis nisl porttitor enim. Cras accumsan felis orci, a sagittis lectus porta ut.",
+        "rating": 4
+      },
+      {
+        "id": 9,
+        "hotelId": 3,
+        "title": "Best holiday ever",
+        "description": "Donec a nisi in mi pellentesque placerat vel eu leo. Nulla facilisi. Duis dui nulla, ornare sed efficitur vitae, aliquam vel tortor. Nunc porta varius ex. Donec id porta lacus, ac facilisis nulla. Proin sed felis nec tellus dictum commodo nec quis lorem. Nam ultrices, risus ut maximus ullamcorper, elit tellus tincidunt tortor, eu euismod turpis ipsum et leo.",
+        "rating": 5
+      },
+      {
+        "id": 11,
+        "hotelId": 3,
+        "title": "Best holiday ever",
+        "description": "Donec a nisi in mi pellentesque placerat vel eu leo. Nulla facilisi. Duis dui nulla, ornare sed efficitur vitae, aliquam vel tortor. Nunc porta varius ex. Donec id porta lacus, ac facilisis nulla. Proin sed felis nec tellus dictum commodo nec quis lorem. Nam ultrices, risus ut maximus ullamcorper, elit tellus tincidunt tortor, eu euismod turpis ipsum et leo.",
+        "rating": 5
+      },
+      {
+        "id": 12,
+        "hotelId": 3,
+        "title": "Very clean",
+        "description": "Duis blandit, dolor sed posuere sodales, diam lorem tempor libero, at vestibulum turpis nisl porttitor enim. Cras accumsan felis orci, a sagittis lectus porta ut.",
+        "rating": 4
+      },
+      {
+        "id": 18,
+        "hotelId": 4,
+        "title": "Exceeded all expectations",
+        "description": "Donec a nisi in mi pellentesque placerat vel eu leo. Nulla facilisi. Duis dui nulla, ornare sed efficitur vitae, aliquam vel tortor. Nunc porta varius ex. Donec id porta lacus, ac facilisis nulla. Proin sed felis nec tellus dictum commodo nec quis lorem. Nam ultrices, risus ut maximus ullamcorper, elit tellus tincidunt tortor, eu euismod turpis ipsum et leo.",
+        "rating": 5
+      },
+      {
+        "id": 20,
+        "hotelId": 4,
+        "title": "Exceeded all expectations",
+        "description": "Donec a nisi in mi pellentesque placerat vel eu leo. Nulla facilisi. Duis dui nulla, ornare sed efficitur vitae, aliquam vel tortor. Nunc porta varius ex. Donec id porta lacus, ac facilisis nulla. Proin sed felis nec tellus dictum commodo nec quis lorem. Nam ultrices, risus ut maximus ullamcorper, elit tellus tincidunt tortor, eu euismod turpis ipsum et leo.",
+        "rating": 5
+      },
+      {
+        "id": 22,
+        "hotelId": 4,
+        "title": "Best holiday ever",
+        "description": "Donec a nisi in mi pellentesque placerat vel eu leo. Nulla facilisi. Duis dui nulla, ornare sed efficitur vitae, aliquam vel tortor. Nunc porta varius ex. Donec id porta lacus, ac facilisis nulla. Proin sed felis nec tellus dictum commodo nec quis lorem. Nam ultrices, risus ut maximus ullamcorper, elit tellus tincidunt tortor, eu euismod turpis ipsum et leo.",
+        "rating": 5
+      }
+]
+
+let arrayHoteles = [
+    {
+        "id": 1,
+        "title": "Downtown Hotel",
+        "thumbnail": "https://picsum.photos/id/369/400/400",
+        "rating": "5",
+        "description": "Integer at pulvinar risus. Morbi bibendum metus sem, quis rhoncus dui vulputate nec. Maecenas bibendum nisl vel scelerisque volutpat. Quisque convallis, sem ac facilisis malesuada, purus nibh pellentesque risus, nec pretium nisl tortor sed ipsum. In vitae lectus imperdiet, hendrerit velit vitae, condimentum elit. Aenean aliquet at sapien sit amet efficitur. Curabitur condimentum fringilla justo a maximus."
+      },
+      {
+        "id": 2,
+        "title": "Fairytail castle",
+        "thumbnail": "https://picsum.photos/id/1040/400/400",
+        "rating": "4",
+        "description": "Nulla condimentum urna et nisl fermentum consectetur. Sed nisl tortor, bibendum at neque a, laoreet vulputate urna. Vestibulum ultrices augue id ligula sollicitudin, et sagittis odio posuere. Nunc fermentum libero quis ex aliquam, vel accumsan nisl tristique. Suspendisse vitae rhoncus mauris. Ut rutrum mollis odio, ultrices scelerisque lacus mattis eget. Praesent lorem nibh, tincidunt vel ultricies ac, tempor sed orci. In hac habitasse platea dictumst. Nulla facilisi. Aenean urna augue, fringilla non dui a, auctor fringilla eros. Suspendisse dui mauris, mollis sit amet mi at, venenatis sagittis urna. Pellentesque interdum ex eget tincidunt hendrerit. Nulla facilisi."
+      },
+      {
+        "id": 3,
+        "title": "Local Bed and Breakfast",
+        "thumbnail": "https://picsum.photos/id/437/400/400",
+        "rating": "3",
+        "description": "Sed vestibulum in enim et consectetur. Cras volutpat sagittis pretium. Sed eget dapibus metus, sit amet laoreet arcu. Praesent gravida odio sed pretium viverra. Suspendisse ornare elit ut purus tristique, tempor accumsan mi eleifend. Integer ultrices velit eu felis dignissim, sed vulputate libero commodo. Donec consequat arcu non consequat sollicitudin."
+      },
+      {
+        "id": 4,
+        "title": "Huge Hotel",
+        "thumbnail": "https://picsum.photos/id/1031/400/400",
+        "rating": "1",
+        "description": "Nulla condimentum urna et nisl fermentum consectetur. Sed nisl tortor, bibendum at neque a, laoreet vulputate urna. Vestibulum ultrices augue id ligula sollicitudin, et sagittis odio posuere. Nunc fermentum libero quis ex aliquam, vel accumsan nisl tristique. Suspendisse vitae rhoncus mauris. Ut rutrum mollis odio, ultrices scelerisque lacus mattis eget. Praesent lorem nibh, tincidunt vel ultricies ac, tempor sed orci. In hac habitasse platea dictumst. Nulla facilisi. Aenean urna augue, fringilla non dui a, auctor fringilla eros. Suspendisse dui mauris, mollis sit amet mi at, venenatis sagittis urna. Pellentesque interdum ex eget tincidunt hendrerit. Nulla facilisi."
+      }
+]
+localStorage.setItem("Lista Hoteles",JSON.stringify(arrayHoteles));
+localStorage.setItem("Lista Review",JSON.stringify(arrayReview));
+
+function crearElements(){
+  let section = document.getElementById("sect-hoteles");
+  section.innerHTML="";
+    if(arrayHoteles === null){
+      arrayHoteles=[];
+    }
+    else{
+      arrayHoteles.forEach(element => {
+        let divHotel = document.createElement("div");
+        divHotel.className="div-hotel"
+        let divImg = document.createElement("div");
+        divImg.className = "div-img";
+        let img = document.createElement("img")
+        img.src=element.thumbnail;
+        let divDescripcion = document.createElement("div");
+        divDescripcion.className = "div-descripcion"
+        let divTitleRating = document.createElement("div");
+        divTitleRating.className = "div-title-rating"
+        let title = document.createElement("h2");
+        title.textContent=element.title;
+        let description = document.createElement("p");
+        description.textContent = element.description;
+
+// Insercciones de elementos a la seccion
+      section.insertAdjacentElement("beforeend",divHotel);
+      divHotel.insertAdjacentElement("beforeend",divImg);
+      divImg.insertAdjacentElement("beforeend",img);
+      divHotel.insertAdjacentElement("beforeend",divDescripcion);
+      divDescripcion.insertAdjacentElement("beforeend",divTitleRating);
+      divTitleRating.insertAdjacentElement("beforeend",title);
+      divDescripcion.insertAdjacentElement("beforeend",description );
+
+      for(i=0; i <  element.rating; i++){
+        let rating = document.createElement("i");
+        rating.className="fa-solid fa-star";
+        divTitleRating.insertAdjacentElement("beforeend",rating);
+
+        let estrellasGris = 5 - element.rating;
+        console.log(estrellasGris);
+      }
+      });
+    }
+}
+
+function rating(){
+  arrayHoteles.forEach(element => {
+    let rating = document.createElement("i");
+  });
+}
+
+document.addEventListener('DOMContentLoaded',crearElements); // (DOMContentLoaded) cuando recargue el contenido del DOM vuelva e imprima
